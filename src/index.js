014a369ac62a208f289682e52d0d77177f8438be
Savaiya <savaiyaankit0@gmail.com>
2026-01-12 18:38:16 +0530

    util code discusssion

--- src/index.js
+++ src/index.js
@@ -11,6 +11,19 @@ dotenv.config({
 });
 
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+            console.log("ERRR: ",error);
+            throw error
+    })
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running at PORT : ${process.env.PORT}`);
+    })
+
+})
+.catch((err)=>{
+    console.log("Monogo DB connection failed!!",err);
+})
 
 
 /*
